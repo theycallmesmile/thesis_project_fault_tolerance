968047519308d071e36d296eb895943bf9a71533
theycallmesmile <killen163@hotmail.com>
2022-11-11 17:19:07 +0100

    -Ability to create end of stream message for benchmarking. -Delays during message creation depending the benchmarking. -Creates its own persistent state after snapshotting.

--- src/producer.rs
+++ src/producer.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use serde::{Deserializer, Serializer};
 
+use std::collections::HashSet;
+
 use std::time::Duration;
 use tokio::time;
 
@@ -10,10 +12,16 @@ use tokio::sync::oneshot;
 
 use std::sync::Arc;
 
+//Consumer-producer module
+
+//Serialization
+use crate::serialization::PersistentTask;
+use crate::serialization::PartialPersistentTask;
+
 //Manager module
 use crate::manager::{Context, self};
 use crate::manager::Task;
-use crate::manager::TaskToManagerMessage;
+use crate::manager::PersistentTaskToManagerMessage;
 
 //Channel module
 use crate::channel::PullChan;
@@ -43,26 +51,40 @@ impl ProducerState {
                         Event::Data(_) => {
                             panic!();
                         }
-                        Event::Marker => {
+                        Event::Marker(marker_id) => {
                             let snapshot_state = ProducerState::S0 {
                                 out0: out0.clone(),
                                 count,
                             };
                             println!("start producer snapshotting");
-                            Shared::<()>::store(SharedState::Producer(snapshot_state), &ctx).await;
-                            out0.push(Event::Marker).await;
+                            let persistent_state = Task::Producer(snapshot_state.clone()).to_partial_persistent_task().await;
 
-                            ProducerState::S0 {
-                                out0,
-                                count,
-                            }
+                            Shared::<()>::persistent_store(persistent_state, marker_id, &ctx).await;
+                            out0.push(Event::Marker(marker_id)).await;
+
+                            snapshot_state
                         }
                         Event::MessageAmount(amount) => {
                             let mut loc_count = count; 
-                            for x in 0..amount {
-                                task::sleep(Duration::from_millis(10)).await;
-                                out0.push(Event::Data(2)).await;
-                                loc_count += 1;
+                            let mut delay_time = 0;
+                            if (amount == 0) {
+                                out0.push(Event::Data(0)).await;
+                                println!("PRODUCER DONE--------------------");
+                            }
+                            else {
+                                if(amount == 200){
+                                    delay_time = 10;
+                                }
+                                else{
+                                    delay_time = 20;
+                                }
+                                for x in 0..amount {
+                                    //println!("sending data");
+                                    task::sleep(Duration::from_millis(delay_time)).await;
+                                    out0.push(Event::Data(2)).await;
+                                    loc_count += 1;
+                                    
+                                }
                             }
                             ProducerState::S0 {
                                 out0,
